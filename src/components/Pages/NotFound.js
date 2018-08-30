6900174de03f89e3042ff1f780a3a194dbf997b8
Theater <theater@TakTahyl-mini.local>
2018-08-29 18:26:16 -0700

    inital commit

---
+++ src/components/Pages/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">
+        <span className="text-danger">404 </span>
+        Page Not Found
+      </h1>
+      <p className="lead">
+        Sorry, the page you requested does not exist. Stupid ass.
+      </p>
+    </div>
+  );
+};
